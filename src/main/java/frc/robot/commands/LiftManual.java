9e6e4dfeb9edfb1acc9b70cb7ddb9aedba30b226
Mary Evelyn McCarthy <emccarthy@columbusschoolforgirls.org>
2019-01-24 19:13:41 -0500

    Applied the Lift to the Joystick

---
+++ src/main/java/frc/robot/commands/LiftManual.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Global;
+import frc.robot.OI;
+import frc.robot.Robot;
+import frc.robot.subsystems.Lift;
+
+public class LiftManual extends Command {
+  public LiftManual() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.lift);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Lift.liftMotor(0);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if (OI.auxCont.getRawAxis(1)<-Global.DEADZONE);{
+      Lift.liftMotor(.9*OI.auxCont.getRawAxis(1));
+    }
+    else if (OI.auxCont.getRawAxis(1)>Global.DEADZONE);{
+      Lift.liftMotor(.4*OI.auxCont.getRawAxis(1));
+    }
+    else (lift.drive(0));{
+    }
+  }
+
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Lift.liftMotor(0);
+
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    Lift.liftMotor(0);
+  }
+
+}
