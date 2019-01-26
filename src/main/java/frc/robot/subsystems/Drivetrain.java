650a529dcef6e59c3960ff2f7acc092a22419145
Mary Evelyn McCarthy <emccarthy@columbusschoolforgirls.org>
2019-01-26 14:24:18 -0500

    Updated drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -19,18 +19,16 @@ import frc.robot.RobotMap;
 /**
  * Got most of the info for this here: https://github.com/frc3946/MecanumDrivetrain/blob/master/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
  */
-public class DriveTrain extends Subsystem {
+public class Drivetrain extends Subsystem {
   public static CANSparkMax leftFront = new CANSparkMax(RobotMap.LEFT_DRIVE_PORT_FRONT, MotorType.kBrushless);
   public static CANSparkMax leftBack = new CANSparkMax(RobotMap.LEFT_DRIVE_PORT_BACK, MotorType.kBrushless);
   public static CANSparkMax rightFront = new CANSparkMax(RobotMap.RIGHT_DRIVE_PORT_FRONT, MotorType.kBrushless);
   public static CANSparkMax rightBack = new CANSparkMax(RobotMap.RIGHT_DRIVE_PORT_BACK, MotorType.kBrushless);
-  public final RobotDrive drive = new RobotDrive(leftFront, leftBack, rightFront, rightBack); //Command is deprecated!!!
+  public final MecanumDrive drive = new MecanumDrive(leftFront, leftBack, rightFront, rightBack); //Command is deprecated!!!
   public static boolean polarMode = true; 
 
-  public DriveTrain () {
-    super();
-    drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-    drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+  public Drivetrain () {
+    
   }
 
   public void mecanumDrive(double x, double y, double rotation, double gyro) {
@@ -38,17 +36,6 @@ public class DriveTrain extends Subsystem {
     SmartDashboard.putNumber("Y", y);
     SmartDashboard.putNumber("Rotation", rotation);
     SmartDashboard.putNumber("Gyro", gyro);
-    if (DriveTrain.polarMode == true) {
-      SmartDashboard.putString("polarMode", "Polar Mode");
-    } else {
-      SmartDashboard.putString("polarMode", "Field Oriented");
-    }
-
-    if (polarMode == true) {
-      drive.mecanumDrive_Polar(Math.sqrt(x * x + y * y), (Math.toDegrees(MathUtils.atan2(y, x)) - 90), rotation);  //What are you trying to accomplish?
-    } else {
-      drive.mecanumDrive_Cartesian(.7 * x, .7 * y, .7 * rotation, gyro);
-    }
   }
 
   public void update () {
